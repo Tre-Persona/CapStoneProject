f824ffe435c07feb7bb880b6ca43d7d1ca54cdb3
Austin Walker <austinsrwalker@gmail.com>
2020-07-02 11:40:53 -0700

    leaflet works

---
+++ app/javascript/components/SimpleLeaflet.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+
+class SimpleLeaflet extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      lat: 32.7157,
+      lng: -117.1611,
+      zoom: 13,
+  }
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
+}
+export default SimpleLeaflet
