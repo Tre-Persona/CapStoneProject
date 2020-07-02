a8f0fc057d1a094e8cf6eba2dd1a31e423d71c73
Austin Walker <austinsrwalker@gmail.com>
2020-07-02 14:04:37 -0700

    leaflet alertsgit add .git add .

--- app/javascript/components/SimpleLeaflet.js
+++ app/javascript/components/SimpleLeaflet.js
@@ -10,11 +10,13 @@ class SimpleLeaflet extends Component {
       zoom: 13,
   }
   }
-
+ handleMapClick = (e) => {
+    alert(e.latlng);
+} 
   render() {
     const position = [this.state.lat, this.state.lng]
     return (
-      <Map center={position} zoom={this.state.zoom}>
+      <Map onclick={this.handleMapClick} center={position} zoom={this.state.zoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
