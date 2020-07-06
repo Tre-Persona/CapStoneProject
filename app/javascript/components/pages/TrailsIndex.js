39a9f7aa0866fad811c0c16fb42dd05a8f768eaa
Jeremy Gabriel <jeremyagabriel@gmail.com>
2020-07-06 10:04:56 -0700

    Removed conflicting files

--- app/javascript/components/pages/TrailsIndex.js
+++ app/javascript/components/pages/TrailsIndex.js
@@ -1,78 +1,40 @@
-import React, { useState, useEffect } from "react"
-import { Button, Form, FormGroup, Label, Input, Container, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react"
+import SimpleLeaflet from '../partials/trailPartials/SimpleLeaflet.js'
+import TrailList from '../partials/trailPartials/TrailList.js'
 
-
-const TrailsIndex = () => {
-
-  const [newTrails, setNewTrails] = useState([])
-  const [newForm, setNewForm] = useState({
-    latitude: "",
-    longitude: ""
-  })
-  // useEffect(() => {
-  //   getTrails()},[])
-
-    const handleChange = (e) =>{
-      setNewForm({
-        ...newForm,
-        [e.target.name]: e.target.value
-      })
+class TrailsIndex extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      trails: [],
     }
-    const handleSubmit = (e) =>{
-      e.preventDefault()
-      getTrails()
-      console.log("newTrails", newTrails);
-    }
-    async function getTrails() {
-      try {
-        //GET data from the backend
-        let response = await fetch(`https://www.hikingproject.com/data/get-trails?lat=${ newForm.latitude }&lon=${ newForm.longitude }&maxDistance=10&key=200805451-d58078a69001bb6f37cb92b68bbebae3`)
-          let data = await response.json();
-          //all good?
-          if(response.status === 200) {
-            //check the console to make sure we have all the trails
-            console.log("data", data.trails)
-            //populate the newTrails state array with data
-            setNewTrails(data.trails)
-          }
-        } catch (err) {
-          console.log(err)
-        }
+  }
+handleMapClick = (e) => {
+    console.log(e)
+    this.getTrails(e.latlng.lat,e.latlng.lng)
+} 
+getTrails = async(lat,lng) => {
+  try {
+    //GET data from the backend
+    let response = await fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=10&key=200805451-d58078a69001bb6f37cb92b68bbebae3`)
+      let data = await response.json();
+      if(response.status === 200) {
+        //check the console to make sure we have all the trails
+        console.log("data", data.trails)
+        //populate the newTrails state array with data
+        this.setState({ trails: data.trails })
       }
-
-      return (
-        <>
-        <h2>All of the Fancy Trails</h2>
-        <Container>
-          <Form>
-            <FormGroup>
-              <Label htmlFor="latitude">Latitude</Label>
-                <Input type="text" name="latitude" value={ newForm.latitude } onChange={ handleChange } placeholder="" />
-              <Label htmlFor="longitude">Longitude</Label>
-                <Input type="text" name="longitude" value={ newForm.longitude } onChange={ handleChange } placeholder="" />
-            </FormGroup>
-            <Button onClick={handleSubmit}>Let's Go Hiking</Button>
-          </Form>
-          <ListGroup>
-            { newTrails.map((trail, index) => {
-              return(
-                <ListGroupItem>
-                  <NavLink to={`/trails/${ trail.id }`}>
-                    <ListGroupItemHeading>{trail.name}</ListGroupItemHeading>
-                  </NavLink>
-                  <img src={trail.imgSmall} />
-                  <ListGroupItemText>
-                  {trail.summary}
-                  </ListGroupItemText>
-                </ListGroupItem>
-              )
-            })}
-          </ListGroup>
-        </Container>
-
-        </>
-      );
+    } catch (err) {
+      console.log(err)
     }
-
-    export default TrailsIndex
+  }
+render(){
+  return (
+    <>
+      <SimpleLeaflet handleClick={this.handleMapClick.bind(this)} />
+      <TrailList trails={this.state.trails}/>
+    </>
+  )
+  }
+} 
+export default TrailsIndex
