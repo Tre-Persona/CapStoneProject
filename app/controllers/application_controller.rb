31a5200ca83f4c9d7bcf93dcb145829a59d71528
Austin Walker <austinsrwalker@gmail.com>
2020-07-08 15:14:09 -0700

    users now have avatars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,10 @@ class ApplicationController < ActionController::Base
   protected
   
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:user_name, :email, :password, :password_confirmation) }
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:user_name, :email, :password, :password_confirmation, :current_password) }
-    end
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:user_name, :email, :password,
+      :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:user_name, :email, :password,
+      :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
+  end
 
 end
