2deec234beb86f2ce8557f17e8861cf73d6370f8
Jeremy Gabriel <jeremyagabriel@gmail.com>
2020-07-08 13:09:13 -0700

    initial commit

--- app/javascript/components/pages/UserFavorites.js
+++ app/javascript/components/pages/UserFavorites.js
@@ -55,18 +55,34 @@ const UserFavorites = props => {
                 <div key={index} className="trails-index-list-wrapper">
 
                   <NavLink to={`/trails/${ trail.id }`}>
-                    <img className="trails-index-list-image" src={trail.imgSmallMed} />
+                    <img alt={`Image opens trail profile for ${trail.name}.`} className="trails-index-list-image" src={trail.imgSmallMed} />
                   </NavLink>
 
                   <div className="trails-index-list-text-box">
-                    <NavLink className="trails-index-list-link" to={`/trails/${ trail.id }`}>
-                      <h4 className="trails-index-list-title">{trail.name}</h4>
-                    </NavLink>
+                    <div className="trails-index-list-text-wrapper">
+                      <NavLink className="trails-index-list-link" to={`/trails/${ trail.id }`}>
+                        <h4 className="trails-index-list-title">{trail.name}</h4>
+                      </NavLink>
 
-                    <p className="trails-index-list-text">
-                      <strong>{trail.location}</strong><br/>
-                      {trail.summary}
-                    </p>
+                      <p className="trails-index-list-location">
+                        {trail.location}
+                      </p>
+                      <div className="trails-index-list-length-wrapper">
+                        <p className="trails-index-list-length">
+                          {trail.length} mi long
+                        </p>
+                      </div>
+                      <p className="trails-index-list-summary">
+                        {trail.summary}
+                      </p>
+                    </div>
+                    <div className="trails-index-list-button-wrapper">
+                      <NavLink to={`/trails/${ trail.id }`}> 
+                        <Button className="trails-index-list-button" aria-label={`Button opens trail profile for ${trail.name}.`}>
+                          More Info
+                        </Button>
+                      </NavLink>
+                    </div>
                   </div>
 
                 </div>
--- app/javascript/components/partials/trailPartials/TrailList.js
+++ app/javascript/components/partials/trailPartials/TrailList.js
@@ -1,30 +1,46 @@
 import React from "react"
-import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap'
+import { Button } from 'reactstrap'
 import { NavLink } from 'react-router-dom'
 
 
 const TrailList = props => {
   //trails needs to be defined
-  const {trails} = props
-  return(
+  const { trails } = props
+  return (
     <div className="trails-index-list-group">
-      { trails.length>0 && trails.map((trail, index) => {
-        return(
+      {trails.length > 0 && trails.map((trail, index) => {
+        return (
           <div key={trail.id} className="trails-index-list-wrapper">
-            
-            <NavLink to={`/trails/${ trail.id }`}>
-              <img className="trails-index-list-image" src={trail.imgSmallMed} />
+
+            <NavLink to={`/trails/${trail.id}`}>
+              <img alt={`Image opens trail profile for ${trail.name}.`} className="trails-index-list-image" src={trail.imgSmallMed} />
             </NavLink>
 
             <div className="trails-index-list-text-box">
-              <NavLink className="trails-index-list-link" to={`/trails/${ trail.id }`}>
-                <h4 className="trails-index-list-title">{trail.name}</h4>
-              </NavLink>
+              <div className="trails-index-list-text-wrapper">
+                <NavLink className="trails-index-list-link" to={`/trails/${trail.id}`}>
+                  <h4 className="trails-index-list-title">{trail.name}</h4>
+                </NavLink>
 
-              <p className="trails-index-list-text">
-                <strong>{trail.location}</strong><br/>
-                {trail.summary}
-              </p>
+                <p className="trails-index-list-location">
+                  {trail.location}
+                </p>
+                <div className="trails-index-list-length-wrapper">
+                  <p className="trails-index-list-length">
+                    {trail.length} mi long
+                        </p>
+                </div>
+                <p className="trails-index-list-summary">
+                  {trail.summary}
+                </p>
+              </div>
+              <div className="trails-index-list-button-wrapper">
+                <NavLink to={`/trails/${trail.id}`}>
+                  <Button className="trails-index-list-button" aria-label={`Button opens trail profile for ${trail.name}.`}>
+                    More Info
+                        </Button>
+                </NavLink>
+              </div>
             </div>
           </div>
         )
